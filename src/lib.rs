9cc638cf55fe856ea46db7af91fc47a6e5c05dde
alexeiddg <a01637405@tec.mx>
2024-06-22 16:13:30 -0600

    Feat: Parse HTML <a> tag links

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use html5ever::tokenizer::{
+    BufferQueue,
+    Tag,
+    TagKind,
+    TagToken,
+    Token,
+    TokenSink,
+    TokenSinkResult,
+    Tokenizer,
+    TokenizerOpts,
+};
+
+use url::{
+    ParseError,
+    Url,
+};
+
+use std::borrow::Borrow;
+
+// Store Links in a vector
+#[derive(Default, Debug)]
+struct LinkQueue {
+    links: Vec<String>
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl TokenSink for LinkQueue {
+    type Handle = ();
+    fn process_token( &mut self, token: Token, line_number: u64) -> TokenSinkResult<Self::Handle> {
+        match token {
+            TagToken( ref tag @ Tag { kind: TagKind::StartTag, .. }, ) => {
+                if tag.name.as_ref() = "a" {
+                    for attribute in tag.attrs.iter() {
+                        if attribute.name.local.as_ref() = "href" {
+                            let url_str: &[u8] = attribute.value.borrow();
+                            self.links.push( String::from_utf8_lossy(url_str).into_owned() )
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+        TokenSinkResult::Continue
     }
 }
