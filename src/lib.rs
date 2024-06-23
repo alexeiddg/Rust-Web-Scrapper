e0918b646b3c56dbbfb6d48373e41c17308b08e8
alexeiddg <a01637405@tec.mx>
2024-06-22 22:55:33 -0600

    Fix: allow link vector as mutable

--- src/lib.rs
+++ src/lib.rs
@@ -17,9 +17,9 @@ impl TokenSink for &mut LinkQueue {
     fn process_token( &mut self, token: Token, line_number: u64) -> TokenSinkResult<Self::Handle> {
         match token {
             TagToken( ref tag @ Tag { kind: TagKind::StartTag, .. }, ) => {
-                if tag.name.as_ref() = "a" {
+                if tag.name.as_ref() == "a" {
                     for attribute in tag.attrs.iter() {
-                        if attribute.name.local.as_ref() = "href" {
+                        if attribute.name.local.as_ref() == "href" {
                             let url_str: &[u8] = attribute.value.borrow();
                             self.links.push( String::from_utf8_lossy(url_str).into_owned() )
                         }
