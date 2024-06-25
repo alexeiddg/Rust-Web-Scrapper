475ffe937757165716c6cd21522c8b98080e9f0e
alexeiddg <a01637405@tec.mx>
2024-06-25 00:37:39 -0600

    Feat: Crawl Functionality and Helper Functions

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,12 @@ use html5ever::tokenizer::{
 };
 use url::{ ParseError, Url, };
 use std::borrow::Borrow;
+use async_std::task;
 use surf;
+
+// Async helper types
+type CrawlResult = Result<(), Box<dyn std::error::Error + Send + Sync + 'static>>;
+type BoxFuture = std::pin::Pin<Box<dyn std::future::Future<Output = CrawlResult> + Send>>;
 
 // Store Links in a vector
 #[derive(Default, Debug)]
@@ -51,4 +56,47 @@ pub fn get_links(url: &Url, page: String) -> Vec<Url> {
             Err(_) => panic!("Relative URL {}", link),
             Ok(url) => url,
         }).collect()
+}
+
+fn box_crawl(pages: Vec<Url>, current: u8, max: u8) -> BoxFuture {
+    Box::pin(crawl(pages, current, max))
+}
+
+async fn crawl(pages: Vec<Url>, current: u8, max: u8) -> CrawlResult {
+    println!("Current Depth: {}, Max Depth: {}", current, max);
+
+    if current < max {
+        println!("Max Depth Reached");
+        return Ok(());
+    }
+
+    let mut tasks = vec![];
+    println!("Crawling: {:?}", pages);
+
+    for url in pages {
+        let task = task::spawn(async move {
+            println!("Getting: {}", url);
+
+            let mut res = surf::get(&url).await?;
+            let body = res.body_string().await?;
+            let links = get_links(&url, body);
+
+            println!("Following: {:?}", links);
+            box_crawl(links, current + 1, max).await
+        });
+        tasks.push(task);
+    }
+
+    for task in tasks.into_iter(){
+        task.await?;
+    }
+
+    Ok(())
+}
+
+fn main() {
+    // Async block to await future
+    task::block_on(async {
+        box_crawl(vec![Url::parse("example.com").unwrap()],1, 2)
+    });
 }
