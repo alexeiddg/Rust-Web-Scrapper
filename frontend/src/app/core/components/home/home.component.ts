928fabb41a4f4e2baa26685f09fa2e89807c661f
alexeiddg <a01637405@tec.mx>
2024-06-28 19:19:21 -0600

    Feat: Hero Mockup

--- frontend/src/app/core/components/home/home.component.ts
+++ frontend/src/app/core/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from "../header/header.component";
+import { HeroComponent } from "../../../shared/components/hero/hero.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
